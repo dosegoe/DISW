75146ea6d294b86bc0b2299e20c60e4c03673f85
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-07-20 22:34:52 -0400

    Mas de exceptionhandler, ahora con retorno de responseentity

--- src/main/java/rest_api/controller/PacienteController.java
+++ src/main/java/rest_api/controller/PacienteController.java
@@ -105,15 +105,9 @@ public class PacienteController{
   }
 
   @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public void handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
-        String name = ex.getName();
-        String type = ex.getRequiredType().getSimpleName();
-        Object value = ex.getValue();
-        String message = String.format("'%s' should be a valid '%s' and '%s' isn't", 
-                                    name, type, value);
-
-        System.out.println(message);
-        // Do the graceful handling
+    public ResponseEntity<String> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return new ResponseEntity<>("Estado no válido", HttpStatus.BAD_REQUEST);
+        
     }
 
 }
