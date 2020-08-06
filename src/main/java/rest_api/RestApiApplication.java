b10fa968a20578d1977d9fd7ed0381d8ddd35eef
Matías Marchant <matias.marchant@sansano.usm.cl>
2020-08-06 15:20:31 -0400

    CORS 3

--- src/main/java/rest_api/RestApiApplication.java
+++ src/main/java/rest_api/RestApiApplication.java
@@ -2,6 +2,12 @@ package rest_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.context.annotation.Bean;
+
+
 
 @SpringBootApplication
 public class RestApiApplication {
@@ -10,4 +16,13 @@ public class RestApiApplication {
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
 }
